dab3cafa1a73489c1f5e0b3ce652034103099900
Kevin Ledesma <46900861+kadmuffin@users.noreply.github.com>
2022-11-16 14:31:03 +0000

    :wrench: Bump version

--- cmd/version/main.go
+++ cmd/version/main.go
@@ -21,7 +21,7 @@ import (
 
 var (
 	// Number is the current version of the program
-	Number = "0.5.7"
+	Number = "0.5.8"
 
 	// VersionCmd is the command for printing the current version
 	VersionCmd = &cobra.Command{
